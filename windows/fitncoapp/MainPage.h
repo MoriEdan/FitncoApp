f42122bcae37ee721b6f0122bf8809913447677c
tangun <tangun@liberyus.com>
2025-06-13 16:32:42 +0300

    Update dependencies and project configuration

---
+++ windows/fitncoapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::fitncoapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::fitncoapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
